a31a3a2df5657e6445ed5406848105edd554d78f
Jared White <jared@jaredwhite.com>
2020-04-25 09:40:02 -0700

    Update scripts in new site template

--- bridgetown-core/lib/site_template/start.js
+++ bridgetown-core/lib/site_template/start.js
@@ -1,8 +1,15 @@
 const concurrently = require('concurrently');
 
+// By default, configure Bridgetown to use port 4001 so Browsersync can use 4000
+// See also Browsersync settings in sync.js
+const port = 4001
+
+/////////////////
+// Concurrently
+/////////////////
 concurrently([
   { command: "yarn webpack-dev", name: "Webpack", prefixColor: "yellow"},
-  { command: "sleep 4; yarn serve", name: "Bridgetown", prefixColor: "green"},
+  { command: "sleep 4; yarn serve --port " + port, name: "Bridgetown", prefixColor: "green"},
   { command: "sleep 8; yarn sync", name: "Live", prefixColor: "blue"}
 ], {
   restartTries: 3,
