d372634af510e12c5fa78de6a361c78f3672bc7c
Jared White <jared@jaredwhite.com>
2020-04-27 12:51:04 -0700

    Add codename to website footer

--- bridgetown-website/plugins/current_version.rb
+++ bridgetown-website/plugins/current_version.rb
@@ -2,16 +2,18 @@ require "bridgetown-core/version"
 
 module BridgetownSite
   class CurrentVersionTag < Liquid::Tag
-
-    def initialize(tag_name, text, tokens)
-      super
-      @text = text
-    end
-
     def render(context)
       "v#{Bridgetown::VERSION}"
+    end
+  end
+
+  class CurrentCodeNameTag < Liquid::Tag
+    def render(context)
+      "#{Bridgetown::CODE_NAME}"
     end
   end
 end
 
 Liquid::Template.register_tag('current_bridgetown_version', BridgetownSite::CurrentVersionTag)
+
+Liquid::Template.register_tag('current_bridgetown_code_name', BridgetownSite::CurrentCodeNameTag)
