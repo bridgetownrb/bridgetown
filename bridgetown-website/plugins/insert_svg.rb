ad35f5b69768353e5e295d22961059c9d522344b
Jared White <jared@jaredwhite.com>
2020-04-28 10:41:29 -0700

    Fix SVG tag so it doesn't import XML headers

--- bridgetown-website/plugins/insert_svg.rb
+++ bridgetown-website/plugins/insert_svg.rb
@@ -1,16 +1,20 @@
+# frozen_string_literal: true
+
 module BridgetownSite
   class InsertSVGTag < Liquid::Tag
-
     def initialize(tag_name, text, tokens)
       super
-      @text = text.gsub("../","").strip
+      @text = text.gsub("../", "").strip
     end
 
     def render(context)
       svg_path = File.join(context.registers[:site].source, "images", @text)
-      File.read(svg_path).strip
+      svg_lines = File.readlines(svg_path).map(&:strip).select do |line|
+        line unless line.start_with?("<!", "<!--?xml")
+      end
+      svg_lines.join
     end
   end
 end
 
-Liquid::Template.register_tag('insert_svg', BridgetownSite::InsertSVGTag)
+Liquid::Template.register_tag("insert_svg", BridgetownSite::InsertSVGTag)
