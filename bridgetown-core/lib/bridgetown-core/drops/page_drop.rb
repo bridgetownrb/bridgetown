4e00ae4b824ed3485782c7fa881d470e70cbbf7e
Jared White <jared@jaredwhite.com>
2020-05-31 01:44:44 -0700

    Add pager method to PageDrop
    Also remove legacy missing liquid drop behavior for Page subclasses

--- bridgetown-core/lib/bridgetown-core/drops/page_drop.rb
+++ bridgetown-core/lib/bridgetown-core/drops/page_drop.rb
@@ -7,7 +7,7 @@ module Bridgetown
 
       mutable false
 
-      def_delegators :@obj, :content, :dir, :name, :path, :url
+      def_delegators :@obj, :content, :dir, :name, :path, :url, :pager
       private def_delegator :@obj, :data, :fallback_data
     end
   end
