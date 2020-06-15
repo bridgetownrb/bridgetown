33faf1ccb37610bf8ce745944241b70959023114
Jared White <jared@jaredwhite.com>
2020-06-15 10:31:10 -0700

    Prepare release of 0.15.0.beta4

--- bridgetown-core/lib/bridgetown-core/tags/render_content.rb
+++ bridgetown-core/lib/bridgetown-core/tags/render_content.rb
@@ -3,6 +3,7 @@
 module Bridgetown
   module Tags
     class BlockRenderTag < Liquid::Block
+      # rubocop:disable Metrics/AbcSize, Metrics/MethodLength
       def render(context)
         context.stack({}) do
           # unindent the incoming text
@@ -36,6 +37,7 @@ module Bridgetown
             .render_tag(context, +"")
         end
       end
+      # rubocop:enable Metrics/AbcSize, Metrics/MethodLength
 
       private
 
--- bridgetown-core/lib/bridgetown-core/version.rb
+++ bridgetown-core/lib/bridgetown-core/version.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
 module Bridgetown
-  VERSION = "0.15.0.beta3"
+  VERSION = "0.15.0.beta4"
   CODE_NAME = "Overlook"
 end
