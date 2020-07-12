ab50ddaf65c33de955c909b99056c4ed2c853445
paramagicdev <konnor5456@gmail.com>
2020-07-11 20:44:06 -0400

    feat(tags): Working on class map implementation

--- bridgetown-core/lib/bridgetown-core/tags/class_map.rb
+++ bridgetown-core/lib/bridgetown-core/tags/class_map.rb
@@ -1,52 +1,75 @@
 # frozen_string_literal: true
-require 'json'
+require 'set'
 
 module Bridgetown
   module Tags
+
+
+    # A ClassMap class is meant to take a hash and append styles based on if the
+    # value is truthy or falsy
+    #
+    # @example
+    #   center-var = true
+    #   small-var = nil
+    #
+    #   # input
+    #   <div class="{% class_map has-centered-text: center-var, is-small: small-var %}">
+    #     Text
+    #   </div>
+    #
+    #   # output
+    #   <div class="has-centered-text">
+    #     Text
+    #   </div>
     class ClassMap < Liquid::Tag
+      # @see https://api.rubyonrails.org/classes/ActiveModel/Type/Boolean.html
+      FALSE_VALUES = [
+        nil, "nil", "NIL", false, 0, "0", :"0", "f", :f, "F", :F, "false",
+        :false, "FALSE", :FALSE
+      ].to_set.freeze
+
       # @param tag_name [String] The name to use for the tag
       # @param input [String] The input to the tag
-      # @param token [Hash] A hash of config tokens for Liquid.
+      # @param tokens [Hash] A hash of config tokens for Liquid.
       #
-      # @example
-      #   center-var = true
-      #   is-small = nil
-      #
-      #   # input
-      #   <div class="{% class_map has-centered-text: center-var, is-small: small-var %}">
-      #     Text
-      #   </div>
-      #
-      #   # output
-      #   <div class="has-centered-text">
-      #     Text
-      #   </div>
       #
       # @return [ClassMap] Returns a ClassMap object
       def initialize(tag_name, input, tokens)
         super
-
-        @hash = JSON.parse(input.strip.to_json)
+        @input = input
       end
 
       def render(context)
-        evaluate_variables(context)
-        @hash.map { |key, variable| key if variable }.compact.join(" ")
+        to_class_ary(@input, context).join(" ")
       end
 
       private
 
-      # @param context [Liquid::Context] The context to run the variable in
-      # @return [Hash] Returns a hash with updated variables
-      def evaluate_variables(context)
-        @hash.transform_values! do |value|
-          lookup = context
+      def to_class_ary(string, context)
+        ary = []
 
-          variable.split(".").each { |value| lookup = lookup[value] }
-
-          lookup || false
+        string.split(/,\s+/).each do |item|
+          kv_pair = item.split(/:\s+/)
+          klass = kv_pair[0]
+          variable = kv_pair[1]
+          variable = find_variable(context, variable)
+          ary.push(klass) unless FALSE_VALUES.include?(variable)
         end
+
+        ary
+      end
+
+      def find_variable(context, variable)
+        lookup = context
+
+        variable.split(".").each do |value|
+          lookup = lookup[value.strip]
+        end
+
+        lookup || nil
       end
     end
   end
 end
+
+Liquid::Template.register_tag("class_map", Bridgetown::Tags::ClassMap)
