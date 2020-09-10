d223c971d69ea8dbf2b449ab9bae12270ca228dc
Jared White <jared@jaredwhite.com>
2020-09-10 10:36:24 -0700

    Switch to using instance_exec

--- bridgetown-builder/lib/bridgetown-builder/dsl/helpers.rb
+++ bridgetown-builder/lib/bridgetown-builder/dsl/helpers.rb
@@ -5,19 +5,18 @@ module Bridgetown
     module DSL
       module Helpers
         def helper(helper_name, method_name = nil, helpers_scope: false, &block)
-          block = if block
-                    if !helpers_scope
-                      self.class.define_method "_#{helper_name}_helper", &block
-                      method("_#{helper_name}_helper")
-                    else
-                      block
-                    end
-                  elsif method_name
-                    method(method_name)
-                  end
-
+          builder_self = self
           m = Module.new
-          m.define_method helper_name, &block
+
+          if block && !helpers_scope
+            m.define_method helper_name do |*args|
+              builder_self.instance_exec(*args, &block)
+            end
+          else
+            block = method(method_name) if method_name
+            m.define_method helper_name, &block
+          end
+
           Bridgetown::RubyTemplateView::Helpers.include(m)
 
           functions << { name: name, filter: m }
