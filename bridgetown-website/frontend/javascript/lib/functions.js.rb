13cf459136724a3a31a5b6ff750fc93715849972
Jared White <jared@jaredwhite.com>
2020-10-13 11:10:05 -0700

    fix incorrect variable names

--- bridgetown-website/frontend/javascript/lib/functions.js.rb
+++ bridgetown-website/frontend/javascript/lib/functions.js.rb
@@ -9,9 +9,9 @@ export add_heading_anchors = ->() do
   if document.body.class_list.contains? "docs"
     document.query_selector_all(".content h2[id], .content h3[id]").each do |heading|
       anchor_link = document.create_element("a")
-      anchor_ink.inner_text = "#"
-      anchor_ink.href = "##{heading.id}"
-      anchor_ink.class_list.add ".heading-anchor".slice(1) # purgecss
+      anchor_link.inner_text = "#"
+      anchor_link.href = "##{heading.id}"
+      anchor_link.class_list.add ".heading-anchor".slice(1) # purgecss
       heading.append_child anchor_link
     end
   end
