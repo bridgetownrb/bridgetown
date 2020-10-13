0581562f35b9a5b81a560a8abedd85cee050d261
Jared White <jared@jaredwhite.com>
2020-10-13 08:04:08 -0700

    fix purgecss config bug

--- bridgetown-website/purgecss.config.js
+++ bridgetown-website/purgecss.config.js
@@ -1,4 +1,4 @@
 module.exports = {
-  content: ['frontend/javascript/**/*.js','frontend/javascript/**/.js.rb','./output/**/*.html'],
+  content: ['frontend/javascript/**/*.js','frontend/javascript/**/*.js.rb','./output/**/*.html'],
   output: "./output/_bridgetown/static/css"
 }
