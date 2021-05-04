923fa31a9d5fcd50c319b3c67cd4564cdbf81f50
Ayush Newatia <ayush@hey.com>
2021-05-04 14:19:19 +0100

    Add comments to new webpack files

--- bridgetown-core/lib/bridgetown-core/commands/webpack/webpack.config.js
+++ bridgetown-core/lib/bridgetown-core/commands/webpack/webpack.config.js
@@ -1,3 +1,18 @@
 const config = require("./webpack.defaults.js")
 
+// Add any overrides to the default webpack config here:
+
+// Eg:
+//
+//  ```
+//    const path = require("path");
+//    config.resolve.modules.push(path.resolve(__dirname, 'frontend', 'components'))
+//    config.resolve.alias.frontendComponents = path.resolve(__dirname, 'frontend', 'components')
+//  ```
+
+
+
+
+////////////////////////////////////////////////////////
+
 module.exports = config
