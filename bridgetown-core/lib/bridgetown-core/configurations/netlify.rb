d485d7d1c5d0ca3397fe0288301b5df786f55af0
Ayush Newatia <ayush@hey.com>
2021-05-14 20:50:27 +0100

    Add line to make netlify script executable (#302)
    * Add line to make netlify script executable
    
    * Rubocop

--- bridgetown-core/lib/bridgetown-core/configurations/netlify.rb
+++ bridgetown-core/lib/bridgetown-core/configurations/netlify.rb
@@ -4,3 +4,4 @@ TEMPLATE_PATH = File.expand_path("./netlify", __dir__)
 
 copy_file "#{TEMPLATE_PATH}/netlify.toml", "netlify.toml"
 copy_file "#{TEMPLATE_PATH}/netlify.sh", "bin/netlify.sh"
+`chmod a+x ./bin/netlify.sh`
