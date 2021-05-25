30f21f90c741a8e32baebbd325f28b8a1e6501c1
Ayush Newatia <ayush@hey.com>
2021-05-25 09:40:06 +0100

    Rubocop

--- bridgetown-core/lib/bridgetown-core/commands/webpack/enable-postcss.rb
+++ bridgetown-core/lib/bridgetown-core/commands/webpack/enable-postcss.rb
@@ -6,5 +6,5 @@ template default_postcss_config, "postcss.config.js"
 template "webpack.defaults.js.erb", "webpack.defaults.js", force: true
 
 packages = %w(postcss@8.3.0 postcss-loader@4.3.0 postcss-flexbugs-fixes postcss-preset-env)
-run "yarn add -D #{packages.join(' ')}"
+run "yarn add -D #{packages.join(" ")}"
 run "yarn remove sass sass-loader"
