a6c2b9a08a755c168cbf5ace3d13b719edef0651
Ayush Newatia <ayush@hey.com>
2021-05-25 09:29:03 +0100

    Install required packages in webpack enable postcss tool

--- bridgetown-core/lib/bridgetown-core/commands/webpack/enable-postcss.rb
+++ bridgetown-core/lib/bridgetown-core/commands/webpack/enable-postcss.rb
@@ -2,3 +2,7 @@
 
 create_file "postcss.config.js"
 template "webpack.defaults.js.erb", "webpack.defaults.js", force: true
+
+packages = %w(postcss@8.3.0 postcss-loader@4.3.0 postcss-flexbugs-fixes postcss-preset-env)
+run "yarn add -D #{packages.join(' ')}"
+run "yarn remove sass sass-loader"
