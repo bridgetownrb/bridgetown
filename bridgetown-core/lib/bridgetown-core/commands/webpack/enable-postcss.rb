643748e4b29e506c039898869c6968fc2c1b9160
Ayush Newatia <ayush@hey.com>
2021-05-30 16:14:29 +0100

    Fix the Bridgetown logger and other test improvements (#328)

--- bridgetown-core/lib/bridgetown-core/commands/webpack/enable-postcss.rb
+++ bridgetown-core/lib/bridgetown-core/commands/webpack/enable-postcss.rb
@@ -3,8 +3,10 @@
 default_postcss_config = File.expand_path("../../../site_template/postcss.config.js.erb", __dir__)
 
 template default_postcss_config, "postcss.config.js"
-template "webpack.defaults.js.erb", "webpack.defaults.js", force: true
+template "webpack.defaults.js.erb", "config/webpack.defaults.js", force: true
 
-packages = %w(postcss@8.3.0 postcss-loader@4.3.0 postcss-flexbugs-fixes postcss-preset-env)
-run "yarn add -D #{packages.join(" ")}"
-run "yarn remove sass sass-loader"
+unless Bridgetown.environment.test?
+  packages = %w(postcss@8.3.0 postcss-loader@4.3.0 postcss-flexbugs-fixes postcss-preset-env)
+  run "yarn add -D #{packages.join(" ")}"
+  run "yarn remove sass sass-loader"
+end
