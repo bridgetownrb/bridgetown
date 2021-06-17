4f292768694509355be799c732afeb6877648565
Ayush Newatia <ayush@hey.com>
2021-06-17 16:41:12 +0100

    Rubocop

--- bridgetown-core/lib/bridgetown-core/configurations/cypress.rb
+++ bridgetown-core/lib/bridgetown-core/configurations/cypress.rb
@@ -1,10 +1,12 @@
 # frozen_string_literal: true
 
+# rubocop:disable Style/RegexpLiteral
+
 # Install packages
 
 packages = %w(cypress start-server-and-test)
-say "Adding the following yarn packages: #{packages.join(' | ')}", :green
-run "yarn add -D #{packages.join(' ')}"
+say "Adding the following yarn packages: #{packages.join(" | ")}", :green
+run "yarn add -D #{packages.join(" ")}"
 
 # Copy cypress files and scripts into place
 
@@ -15,3 +17,5 @@ script_regex = /"scripts": {(\s+".*,?)*/
 inject_into_file("package.json", ",\n" + cypress_scripts, after: script_regex)
 
 directory in_templates_dir("cypress_dir"), "cypress"
+
+# rubocop:enable Style/RegexpLiteral
