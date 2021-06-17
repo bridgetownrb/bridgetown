816d5edd06e5e483e259ddea516e3717d20f31fa
Ayush Newatia <ayush@hey.com>
2021-06-17 16:38:27 +0100

    Add cypress configuration

---
+++ bridgetown-core/lib/bridgetown-core/configurations/cypress.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+# Install packages
+
+packages = %w(cypress start-server-and-test)
+say "Adding the following yarn packages: #{packages.join(' | ')}", :green
+run "yarn add -D #{packages.join(' ')}"
+
+# Copy cypress files and scripts into place
+
+copy_file in_templates_dir("cypress.json"), "cypress.json"
+
+cypress_scripts = File.read(in_templates_dir("cypress_scripts"))
+script_regex = /"scripts": {(\s+".*,?)*/
+inject_into_file("package.json", ",\n" + cypress_scripts, after: script_regex)
+
+directory in_templates_dir("cypress_dir"), "cypress"
