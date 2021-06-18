b904c9af029c312dec89ff5b95307e0537758f14
Ayush Newatia <ayush@hey.com>
2021-06-18 15:51:57 +0100

    Add webpack merge docs  to webpack config

--- bridgetown-core/lib/bridgetown-core/commands/webpack/webpack.config.js
+++ bridgetown-core/lib/bridgetown-core/commands/webpack/webpack.config.js
@@ -1,7 +1,8 @@
-const config = require("./config/webpack.defaults.js")
+var config = require("./config/webpack.defaults.js")
+const { merge } = require('webpack-merge')
 
 // Add any overrides to the default webpack config here:
-
+//
 // Eg:
 //
 //  ```
@@ -9,6 +10,17 @@ const config = require("./config/webpack.defaults.js")
 //    config.resolve.modules.push(path.resolve(__dirname, 'frontend', 'components'))
 //    config.resolve.alias.frontendComponents = path.resolve(__dirname, 'frontend', 'components')
 //  ```
+//
+// You can also merge in custom config using the included `webpack-merge` package.
+// Complete docs available at: https://github.com/survivejs/webpack-merge
+//
+// Eg:
+//
+//  ```
+//    const customConfig = { ..... }
+//    config = merge(config, customConfig)
+//  ```
+
 
 
 
