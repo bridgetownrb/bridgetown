e1efe5fd58c8b21fd17afd40c98c9a2e36dace33
Ayush Newatia <ayush@hey.com>
2021-08-16 20:45:11 +0100

    Fix syntax

--- bridgetown-core/lib/bridgetown-core/configurations/tailwindcss.rb
+++ bridgetown-core/lib/bridgetown-core/configurations/tailwindcss.rb
@@ -23,7 +23,7 @@ run "bundle exec bridgetown configure purgecss"
 
 if File.exist?("frontend/styles/index.css")
   prepend_to_file "frontend/styles/index.css",
-                  File.read(in_templates_dir("css_imports.css")
+                  File.read(in_templates_dir("css_imports.css"))
 else
   say "\nPlease add the following lines to your CSS index file:"
   say File.read(in_templates_dir("/css_imports.css"))
