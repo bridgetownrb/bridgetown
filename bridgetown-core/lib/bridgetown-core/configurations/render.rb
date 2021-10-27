e9d75330f452f062e858345b3a568aaef5372f5a
Ayush Newatia <ayush@hey.com>
2021-10-28 00:59:12 +0100

    update syntax in render config

--- bridgetown-core/lib/bridgetown-core/configurations/render.rb
+++ bridgetown-core/lib/bridgetown-core/configurations/render.rb
@@ -1,8 +1,6 @@
 # frozen_string_literal: true
 
-TEMPLATE_PATH = File.expand_path("./render", __dir__)
-
 @render_service_name = ask "What would like to call your Render service?"
-template "#{TEMPLATE_PATH}/render.yaml.erb", "render.yaml"
+template in_templates_dir("render.yaml.erb"), "render.yaml"
 
 say "All done. Just create a new blueprint from the Render dashboard and connect this repo."
