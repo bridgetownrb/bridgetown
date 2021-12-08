75594c0650b00876d3b200264318acf15cf4d4de
Jared White <jared@jaredwhite.com>
2021-12-08 13:56:27 -0800

    Improve the Tailwind CSS bundled configuration

--- bridgetown-core/lib/bridgetown-core/configurations/tailwindcss.rb
+++ bridgetown-core/lib/bridgetown-core/configurations/tailwindcss.rb
@@ -11,15 +11,22 @@ unless File.exist?("postcss.config.js")
   return
 end
 
+say_status :tailwind, "Installing Tailwind CSS..."
+
 confirm = ask "This configuration will ovewrite your existing #{"postcss.config.js".bold.white}. Would you like to continue? [Yn]"
 return unless confirm.casecmp?("Y")
 
 run "yarn add -D tailwindcss"
 run "npx tailwindcss init"
 
-copy_file in_templates_dir("postcss.config.js"), "postcss.config.js", force: true
+gsub_file "tailwind.config.js", "purge: [],", <<~JS.strip
+  purge: [
+      './src/**/*.{html,md,liquid,erb,serb}',
+      './frontend/javascript/**/*.js',
+    ],
+JS
 
-run "bundle exec bridgetown configure purgecss"
+copy_file in_templates_dir("postcss.config.js"), "postcss.config.js", force: true
 
 if File.exist?("frontend/styles/index.css")
   prepend_to_file "frontend/styles/index.css",
@@ -28,5 +35,8 @@ else
   say "\nPlease add the following lines to your CSS index file:"
   say File.read(in_templates_dir("/css_imports.css"))
 end
+
+say_status :tailwind, "Tailwind CSS is now configured."
+say_status :tailwind, "When you deploy, ensure NODE_ENV is set to `production` so unused classes are purged from the output CSS bundle."
 
 # rubocop:enable all
