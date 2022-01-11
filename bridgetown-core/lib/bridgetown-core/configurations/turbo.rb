86421c95f95fa3231805cd1604653673860dcfb0
Jared White <jared@jaredwhite.com>
2022-01-10 21:10:44 -0800

    Remove the Swup bundled configuration and enhance Turbo instead (#471)

--- bridgetown-core/lib/bridgetown-core/configurations/turbo.rb
+++ bridgetown-core/lib/bridgetown-core/configurations/turbo.rb
@@ -1,16 +1,26 @@
 # frozen_string_literal: true
 
-say "Installing Turbo...", :green
+say_status :turbo, "Installing Turbo..."
 
 run("yarn add @hotwired/turbo")
 
-say 'Adding Turbo to "frontend/javascript/index.js"...', :magenta
+say_status :turbo, 'Adding Turbo to "frontend/javascript/index.js"...', :magenta
 
 javascript_import do
   <<~JS
     import * as Turbo from "@hotwired/turbo"
+
+    // Uncomment the line below to add transition animations when Turbo navigates.
+    // We recommend adding <meta name="turbo-cache-control" content="no-preview" />
+    // to your HTML head if you turn on transitions. Use data-turbo-transition="false"
+    // on your <main> element for pages where you don't want any transition animation.
+    //
+    // import "./turbo_transitions.js"
   JS
 end
 
-say "Turbo successfully added", :green
-say 'For further reading, check out "https://turbo.hotwired.dev/"', :blue
+copy_file in_templates_dir("turbo_transitions.js"), "frontend/javascript/turbo_transitions.js"
+
+say_status :turbo, "Turbo successfully added!", :magenta
+say_status :turbo, "Take a look in your index.js file for optional animation setup.", :blue
+say_status :turbo, 'For further reading, check out "https://turbo.hotwired.dev/"', :blue
