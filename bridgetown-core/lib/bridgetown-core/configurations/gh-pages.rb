d5cc6be1085c16e16664209cb71d01edf2d3f48f
Ayush Newatia <ayush@hey.com>
2022-02-18 19:55:28 +0000

    shut up rubocop

--- bridgetown-core/lib/bridgetown-core/configurations/gh-pages.rb
+++ bridgetown-core/lib/bridgetown-core/configurations/gh-pages.rb
@@ -3,6 +3,7 @@
 `bundle lock --add-platform x86_64-linux`
 copy_file in_templates_dir("gh-pages.yml"), ".github/workflows/gh-pages.yml"
 
+# rubocop:disable Layout/LineLength
 say "🎉 A GitHub action to deploy your site to GitHub pages has been configured!"
 say ""
 
@@ -11,3 +12,4 @@ say ""
 
 say "You'll likely also need to set `base_path` in your `bridgetown.config.yml` to your repository's name. If you do this you'll need to use the `relative_url` helper for all links and assets in your HTML."
 say ""
+# rubocop:enable Layout/LineLength
