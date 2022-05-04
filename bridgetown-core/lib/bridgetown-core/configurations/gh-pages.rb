1d086dfbcf141a32b21741f802765f6120f88c45
Alessandro Rodi <coorasse@gmail.com>
2022-05-04 11:04:57 +0200

    invalid multibyte char (US-ASCII) - Remove emojis?
    I stumbled upon this error the first time I tried to run `/bin/bridgetown configure gh-pages`:
    
    ```
    [...]
    in `instance_eval': /Users/coorasse/.rbenv/versions/3.1.0/lib/ruby/gems/3.1.0/gems/bridgetown-core-1.0.0/lib/bridgetown-core/configurations/gh-pages.rb:7: invalid multibyte char (US-ASCII) (SyntaxError)
    [...]
    ```

--- bridgetown-core/lib/bridgetown-core/configurations/gh-pages.rb
+++ bridgetown-core/lib/bridgetown-core/configurations/gh-pages.rb
@@ -4,10 +4,10 @@
 copy_file in_templates_dir("gh-pages.yml"), ".github/workflows/gh-pages.yml"
 
 # rubocop:disable Layout/LineLength
-say "🎉 A GitHub action to deploy your site to GitHub pages has been configured!"
+say "A GitHub action to deploy your site to GitHub pages has been configured!"
 say ""
 
-say "🛠️  After pushing the action, go to your repository settings and configure GitHub Pages to deploy from the branch `gh-pages`."
+say "After pushing the action, go to your repository settings and configure GitHub Pages to deploy from the branch `gh-pages`."
 say ""
 
 say "You'll likely also need to set `base_path` in your `bridgetown.config.yml` to your repository's name. If you do this you'll need to use the `relative_url` helper for all links and assets in your HTML."
