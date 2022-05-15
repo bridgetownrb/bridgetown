33304fa0d8a072accfd2c4df0c403f575d9c0a2e
Jared White <jared@whitefusion.studio>
2022-05-15 11:38:30 -0700

    Add Markdown support to convert `==` / `::` to `<mark>` (#551)
    * Patch GFM parser to support `==` / `::` for `<mark>`
    
    * Add highlighting test

---
+++ bridgetown-core/lib/bridgetown-core/kramdown/parser/gfm.rb
@@ -0,0 +1,36 @@
+# Frozen-string-literal: true
+
+require "kramdown-parser-gfm"
+
+module Kramdown
+  module Parser
+    class GFM
+      MARK_DELIMITER = %r{(==|::)+}.freeze
+      MARK_MATCH = %r{#{MARK_DELIMITER}(?!\s|=|:).*?[^\s=:]#{MARK_DELIMITER}}m.freeze
+
+      # Monkey-patch GFM initializer to add our new mark parser
+      alias_method :_old_initialize, :initialize
+      def initialize(source, options)
+        _old_initialize(source, options)
+        @span_parsers << :mark if @options[:mark_highlighting]
+      end
+
+      def parse_mark
+        line_number = @src.current_line_number
+
+        @src.pos += @src.matched_size
+        el = Element.new(:html_element, "mark", {}, category: :span, line: line_number)
+        @tree.children << el
+
+        env = save_env
+        reset_env(src: Kramdown::Utils::StringScanner.new(@src.matched[2..-3], line_number),
+                  text_type: :text)
+        parse_spans(el)
+        restore_env(env)
+
+        el
+      end
+      define_parser(:mark, MARK_MATCH)
+    end
+  end
+end
