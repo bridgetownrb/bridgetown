337533cbf7aafa2de9221f360e3778ea13ad61e2
Andrew Mason <andrewmcodes@protonmail.com>
2022-05-25 21:24:01 +0000

    feat: add rb files to the Tailwind content glob
    Components, plugins, helpers, data files, etc. that are written in Ruby may contain Tailwind CSS classes that won't get generated by the JIT because they aren't in the content scope. This commit updates the Tailwind content glob to also include ruby files when searching for classes in the src folder.

--- bridgetown-core/lib/bridgetown-core/configurations/tailwindcss.rb
+++ bridgetown-core/lib/bridgetown-core/configurations/tailwindcss.rb
@@ -21,7 +21,7 @@ run "npx tailwindcss init"
 
 gsub_file "tailwind.config.js", "content: [],", <<~JS.strip
   content: [
-      './src/**/*.{html,md,liquid,erb,serb}',
+      './src/**/*.{html,md,liquid,erb,serb,rb}',
       './frontend/javascript/**/*.js',
     ],
 JS
@@ -59,7 +59,7 @@ create_builder "tailwind_jit.rb" do
         hook :site, :pre_reload do |_, paths|
           # Don't trigger refresh if it's a frontend-only change
           next if paths.length == 1 && paths.first.ends_with?("manifest.json")
-    
+
           # Save out a comment file to trigger Tailwind's JIT
           refresh_file = site.in_root_dir("frontend", "styles", "jit-refresh.css")
           File.write refresh_file, "/* \#{Time.now.to_i} */"
