eecf3553c670c53ad481dbb849f0c04554b8b3f5
Andrew Mason <andrewmcodes@protonmail.com>
2022-11-18 16:40:31 -0700

    fix: update broken initializers documentation link
    Replace documentation link in the initializers site template file that points to the wrong URL.

--- bridgetown-core/lib/site_template/config/initializers.rb
+++ bridgetown-core/lib/site_template/config/initializers.rb
@@ -39,5 +39,5 @@ Bridgetown.configure do |config|
   #
 
   # For more documentation on how to configure your site using this initializers file,
-  # visit: https://edge.bridgetownrb.com/documentation/configuration/initializers/
+  # visit: https://edge.bridgetownrb.com/docs/configuration/initializers/
 end
