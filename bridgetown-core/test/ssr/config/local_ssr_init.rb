a80f60845ee238ad32cdc01e26631afddbc1e8de
Jared White <jared@jaredwhite.com>
2023-01-24 10:08:45 -0800

    fix: load `initializers.rb` instead of require for working tests

---
+++ bridgetown-core/test/ssr/config/local_ssr_init.rb
@@ -0,0 +1,8 @@
+Bridgetown.initializer :local_ssr_init do |config|
+  config.init :ssr do
+    setup ->(site) do
+      site.data.iterations ||= 0
+      site.data.iterations += 1
+    end
+  end
+end
