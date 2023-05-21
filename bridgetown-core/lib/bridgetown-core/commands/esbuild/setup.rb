def7bc666ba90b31386835475856287a4736c818
Jared White <jared@jaredwhite.com>
2023-05-20 19:06:32 -0700

    update to esbuild 0.17 and rework the path alias mechanism

--- bridgetown-core/lib/bridgetown-core/commands/esbuild/setup.rb
+++ bridgetown-core/lib/bridgetown-core/commands/esbuild/setup.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
 
 template "esbuild.defaults.js.erb", "config/esbuild.defaults.js"
+copy_file "jsconfig.json"
 copy_file "esbuild.config.js", force: true
--- bridgetown-core/lib/bridgetown-core/commands/esbuild/update.rb
+++ bridgetown-core/lib/bridgetown-core/commands/esbuild/update.rb
@@ -1,4 +1,7 @@
 # frozen_string_literal: true
 
 template "esbuild.defaults.js.erb", "config/esbuild.defaults.js", force: true
+copy_file "jsconfig.json"
 say "🎉 esbuild configuration updated successfully!"
+say "You may need to add `$styles/` to the front of your main CSS imports."
+say "See https://www.bridgetownrb.com/docs/frontend-assets#esbuild-setup for details."
