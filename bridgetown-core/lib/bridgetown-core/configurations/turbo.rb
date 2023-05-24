c9deeb5edc54ac7eea6afb11cabfb8df792218e4
Jared White <jared@jaredwhite.com>
2023-05-24 09:28:12 -0700

    Add Turbo Shadow support in the Turbo automation

--- bridgetown-core/lib/bridgetown-core/configurations/turbo.rb
+++ bridgetown-core/lib/bridgetown-core/configurations/turbo.rb
@@ -3,6 +3,7 @@
 say_status :turbo, "Installing Turbo..."
 
 run("yarn add @hotwired/turbo")
+run("yarn add turbo-shadow")
 
 say_status :turbo, 'Adding Turbo to "frontend/javascript/index.js"...', :magenta
 
@@ -10,11 +11,17 @@ javascript_import do
   <<~JS
     import * as Turbo from "@hotwired/turbo"
 
-    // Uncomment the line below to add transition animations when Turbo navigates.
-    // We recommend adding <meta name="turbo-cache-control" content="no-preview" />
-    // to your HTML head if you turn on transitions. Use data-turbo-transition="false"
-    // on your <main> element for pages where you don't want any transition animation.
-    //
+    /**
+     * Adds support for declarative shadow DOM. Requires your HTML <head> to include:
+     * `<meta name="turbo-cache-control" content="no-cache" />`
+     */
+    import * as TurboShadow from "turbo-shadow"
+
+    /**
+     * Uncomment the line below to add transition animations when Turbo navigates.
+     * Use data-turbo-transition="false" on your <main> element for pages where
+     * you don't want any transition animation.
+     */
     // import "./turbo_transitions.js"
   JS
 end
@@ -22,5 +29,8 @@ end
 copy_file in_templates_dir("turbo_transitions.js"), "frontend/javascript/turbo_transitions.js"
 
 say_status :turbo, "Turbo successfully added!", :magenta
-say_status :turbo, "Take a look in your index.js file for optional animation setup.", :blue
-say_status :turbo, 'For further reading, check out "https://turbo.hotwired.dev/"', :blue
+say_status :turbo, "For declarative shadow DOM support, you will need to update", :blue
+say_status :turbo, "your HTML <head> to add the following code:", :blue
+say %(<meta name="turbo-cache-control" content="no-cache" />)
+say_status :turbo, "Check out your index.js file for optional animation setup.", :blue
+say_status :turbo, 'For further reading, visit "https://turbo.hotwired.dev/"', :blue
