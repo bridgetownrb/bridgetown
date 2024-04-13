cc731c14fdec9ecaedba1186be21565f160c6db2
Jared White <jared@jaredwhite.com>
2024-04-13 09:21:33 -0700

    fix cop

--- bridgetown-core/lib/bridgetown-core/utils/loaders_manager.rb
+++ bridgetown-core/lib/bridgetown-core/utils/loaders_manager.rb
@@ -29,7 +29,7 @@ module Bridgetown
       end
 
       def setup_loaders(autoload_paths = []) # rubocop:todo Metrics/AbcSize, Metrics/CyclomaticComplexity, Metrics/MethodLength, Metrics/PerceivedComplexity
-        (autoload_paths.presence || config.autoload_paths).each do |load_path| # rubocop:todo Metrics/BlockLength
+        (autoload_paths.presence || config.autoload_paths).each do |load_path|
           if @loaders.key?(load_path)
             raise "Zeitwerk loader already added for `#{load_path}'. Please check your config"
           end
