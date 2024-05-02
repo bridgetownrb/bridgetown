8bc722484f7b567073995bed4cffdd29736c5ccd
Jared White <jared@jaredwhite.com>
2024-05-02 07:45:31 -0700

    Test additional code paths

--- bridgetown-core/test/ssr/server/routes/render_resource.rb
+++ bridgetown-core/test/ssr/server/routes/render_resource.rb
@@ -5,7 +5,7 @@ class Routes::RenderResource < Bridgetown::Rack::Routes
     # route: GET /render_resource
     r.get "render_resource" do
       # Roda should know how to autorender the resource
-      bridgetown_site.collections.pages.resources.find { _1.id == "repo://pages.collection/index.md" }
+      site.collections.pages.find { _1.id == "repo://pages.collection/index.md" }
     end
 
     r.get "render_model" do
