6a2a8a068aa54157299fb7885645008fb1c58f9c
Jared White <jared@jaredwhite.com>
2024-07-14 07:47:04 -0700

    fix bad merge

--- bridgetown-core/lib/bridgetown-core/configuration.rb
+++ bridgetown-core/lib/bridgetown-core/configuration.rb
@@ -74,7 +74,7 @@ module Bridgetown
       "available_locales"            => [:en],
       "default_locale"               => :en,
       "prefix_default_locale"        => false,
-      "permalink"                    => nil, # default is set according to content engine
+      "permalink"                    => "pretty",
       "timezone"                     => nil, # use the local timezone
 
       "quiet"                        => false,
