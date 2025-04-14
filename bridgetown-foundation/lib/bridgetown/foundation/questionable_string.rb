54394939622bd70c60ca3dcfbebeae327e77679d
Jared White <jared@whitefusion.studio>
2025-04-14 13:01:36 -0700

    Pass options hash as keyword arguments to the `partial` method (#994)
    * Pass options hash as keyword arguments
    
    Fixes #986
    
    * rubocop fix

--- bridgetown-foundation/lib/bridgetown/foundation/questionable_string.rb
+++ bridgetown-foundation/lib/bridgetown/foundation/questionable_string.rb
@@ -4,9 +4,7 @@ module Bridgetown::Foundation
   class QuestionableString < ::String
     def method_missing(method_name, *args)
       value = method_name.to_s
-      if value.end_with?("?")
-        return self == value.chop
-      end
+      return self == value.chop if value.end_with?("?")
 
       super
     end
