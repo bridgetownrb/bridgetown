e593a70f2d926b382c6b65e32a09c51c2ee25385
Jared White <jared@whitefusion.studio>
2025-09-02 23:05:37 -0700

    docs: updated information on adding Thor commands, Render hybrid deploys (#1012)
    also fixed an issue for `npm_add` gemspec metadata
    
    Resolves #905 #990

--- bridgetown-core/lib/bridgetown-core/configurations/render.rb
+++ bridgetown-core/lib/bridgetown-core/configurations/render.rb
@@ -3,4 +3,11 @@
 @render_service_name = ask "What would like to call your Render service?"
 template in_templates_dir("render.yaml.erb"), "render.yaml"
 
-say "All done. Just create a new blueprint from the Render dashboard and connect this repo."
+say_status :render, "Your render.yaml file is now configured!"
+say ""
+say "Verify its contents and once ready, create a new blueprint from the Render dashboard"
+say "and connect your repo."
+say ""
+say "Optionally, if you're setting up a backend API and database, create a environment group"
+say "on Render called [yourservicehere]-prod-envs for sharing environment variables between"
+say "multiple servers."
