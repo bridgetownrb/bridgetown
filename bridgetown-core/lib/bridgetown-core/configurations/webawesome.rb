dc1aad1cdac2a2c9d0bad4cfe0f78981e513089d
Felipe Vogel <fps.vogel@gmail.com>
2025-09-18 23:40:28 -0400

    feat: update webawesome.rb

--- bridgetown-core/lib/bridgetown-core/configurations/webawesome.rb
+++ bridgetown-core/lib/bridgetown-core/configurations/webawesome.rb
@@ -1,60 +1,38 @@
 # frozen_string_literal: true
 
-say_status :shoelace, "Installing Shoelace..."
+say_status :webawesome, "Installing Web Awesome..."
 
-add_npm_package "@shoelace-style/shoelace"
+add_npm_package "@awesome.me/webawesome"
 
 stylesheet_import = <<~CSS
-  /* Import the base Shoelace stylesheet: */
-  @import "@shoelace-style/shoelace/dist/themes/light.css";
+  /* Import the base Web Awesome stylesheet: */
+  @import "@awesome.me/webawesome/dist/styles/webawesome.css";
 
 CSS
 
 if File.exist?("frontend/styles/index.css")
+  say 'Adding Web Awesome stylesheet import to "frontend/styles/index.css"...', :magenta
   prepend_to_file "frontend/styles/index.css", stylesheet_import
 elsif File.exist?("frontend/styles/index.scss")
+  say 'Adding Web Awesome stylesheet import to "frontend/styles/index.scss"...', :magenta
   prepend_to_file "frontend/styles/index.scss", stylesheet_import
 else
   say "\nPlease add the following lines to your CSS index file:"
   say stylesheet_import
 end
 
-say 'Adding Shoelace to "frontend/javascript/index.js"...', :magenta
+say 'Adding Web Awesome component imports to "frontend/javascript/index.js"...', :magenta
 
 javascript_import do
   <<~JS
 
-    // Example Shoelace components. Mix 'n' match however you like!
-    import "@shoelace-style/shoelace/dist/components/button/button.js"
-    import "@shoelace-style/shoelace/dist/components/icon/icon.js"
-    import "@shoelace-style/shoelace/dist/components/spinner/spinner.js"
-
-    // Use the public icons folder:
-    import { setBasePath } from "@shoelace-style/shoelace/dist/utilities/base-path.js"
-    setBasePath("/shoelace-assets")
+    // Example Web Awesome components. Mix 'n' match however you like!
+    import "@awesome.me/webawesome/dist/components/button/button.js"
+    import "@awesome.me/webawesome/dist/components/icon/icon.js"
+    import "@awesome.me/webawesome/dist/components/spinner/spinner.js"
   JS
 end
 
-say "Updating frontend build commands...", :magenta
+say_status :webawesome, "Web Awesome is now configured!"
 
-insert_into_file "package.json", before: '    "esbuild": "node' do
-  <<-JS
-  "shoelace:copy-assets": "mkdir -p src/shoelace-assets && cp -r node_modules/@shoelace-style/shoelace/dist/assets src/shoelace-assets",
-  JS
-end
-gsub_file "package.json", %r{"esbuild": "node}, '"esbuild": "npm run shoelace:copy-assets && node'
-gsub_file "package.json", %r{"esbuild-dev": "node},
-          '"esbuild-dev": "npm run shoelace:copy-assets && node'
-
-if File.exist?(".gitignore")
-  append_to_file ".gitignore" do
-    <<~FILES
-
-      src/shoelace-assets
-    FILES
-  end
-end
-
-say_status :shoelace, "Shoelace is now configured!"
-
-say 'For further reading, check out "https://shoelace.style"', :blue
+say 'For further reading, check out "https://webawesome.com"', :blue
