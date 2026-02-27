03c34551b5d85a5fd65b2bd4387b9d294eba139b
Ayush Newatia <ayush@radioactivetoy.tech>
2026-02-27 12:09:26 +0000

    Remove puma specific code

--- bridgetown-core/lib/bridgetown-core/commands/start.rb
+++ bridgetown-core/lib/bridgetown-core/commands/start.rb
@@ -15,10 +15,6 @@ module Bridgetown
       server.to_s.split("::").last
     end
 
-    def using_puma?
-      name == "Puma"
-    end
-
     def serveable?
       server
       true
@@ -58,7 +54,7 @@ module Bridgetown
         # Load Bridgetown configuration into thread memory
         bt_options = configuration_with_overrides(options)
         bt_options.port = port = load_env_and_determine_port(bt_options, options)
-        # TODO: support Puma serving HTTPS directly?
+        # TODO: support Falcon serving HTTPS directly
         bt_bound_url = "http://#{bt_options.bind}:#{port}"
 
         # Set a local site URL in the config if one is not available
@@ -74,7 +70,7 @@ module Bridgetown
           if server.serveable?
             pid_tracker.create_pid_dir
 
-            bt_options.skip_live_reload ||= !server.using_puma?
+            bt_options.skip_live_reload ||= false
 
             build_args = ["-w"] + Array(ARGV[1..])
             build_pid = Process.fork { Bridgetown::Commands::Build[*build_args].() }
