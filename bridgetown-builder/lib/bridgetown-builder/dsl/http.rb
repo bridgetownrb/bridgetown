71a30c979d91c4e165034ce093c13c62f60981d0
Felipe Vogel <fps.vogel@gmail.com>
2026-04-24 17:28:28 -0400

    Deprecate Bridgetown::Builders::DSL::HTTP

--- bridgetown-builder/lib/bridgetown-builder/dsl/http.rb
+++ bridgetown-builder/lib/bridgetown-builder/dsl/http.rb
@@ -7,6 +7,11 @@ module Bridgetown
     module DSL
       module HTTP
         def get(url, headers: {}, parse_json: true, **params)
+          Deprecator.deprecation_message(
+            "The DSL for HTTP requests (`get`) is deprecated and will be " \
+            "removed in Bridgetown 3.0. Use an HTTP client such as HTTPX: " \
+            "https://honeyryderchuck.gitlab.io/httpx"
+          )
           body = begin
             connection(parse_json:).get(url, params, headers).body
           rescue Faraday::ParsingError
@@ -20,6 +25,19 @@ module Bridgetown
         end
 
         def connection(headers: {}, parse_json: true)
+          callers = caller_locations.map { [_1.base_label, _1.path] }
+          this_file_path = "bridgetown-builder/lib/bridgetown-builder/dsl/http.rb"
+          called_from_get = callers.any? do |base_label, path|
+            base_label == "get" && path.end_with?(this_file_path)
+          end
+          unless called_from_get
+            Deprecator.deprecation_message(
+              "The DSL for HTTP requests (`connection`) is deprecated and will be " \
+              "removed in Bridgetown 3.0. Use an HTTP client such as HTTPX: " \
+              "https://honeyryderchuck.gitlab.io/httpx"
+            )
+          end
+
           headers["Content-Type"] = "application/json" if parse_json
 
           Faraday.new(headers:) do |faraday|
